5e49b7c25dfd055e4639fbb686a056b24c808cb4
gennttllee <udooto72@gmail.com>
2022-08-07 03:58:50 +0100

    finally fixed cors wahala

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import  express  from "express";
-import  bodyParser  from "body-parser";
+import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -9,11 +9,19 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({limit : '30mb', extended : true}));
-app.use(bodyParser.urlencoded({limit : '30mb', extended : true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors({
-    origin: "*",
+    origin: "https://memory-gennttllee.vercel.app/"
 }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://memory-gennttllee.vercel.app/");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
@@ -21,7 +29,7 @@ const CONNECTION_URL = process.env.CONNECTION;
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useUnifiedTopology : true, useNewUrlParser: true })
-    .then(() =>app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+mongoose.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
