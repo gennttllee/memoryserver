7b3a7140f96c5f6a3bf27c35ef1feab291861ff6
gennttllee <udooto72@gmail.com>
2022-08-07 05:19:43 +0100

    finally getting weird

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
-        "Access-Control-Allow-Headers",
+        'Access-Control-Allow-Origin',
         "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
