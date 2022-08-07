b48e8e020c0ce6ab4b88014fab82f5ea35f7ff8d
gennttllee <udooto72@gmail.com>
2022-08-07 02:02:41 +0100

    change cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import  express  from "express";
-import  bodyParser  from "body-parser";
+import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -9,9 +9,11 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({limit : '30mb', extended : true}));
-app.use(bodyParser.urlencoded({limit : '30mb', extended : true}));
-app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors({
+    origin: '*'
+}));
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
@@ -19,7 +21,7 @@ const CONNECTION_URL = process.env.CONNECTION;
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useUnifiedTopology : true})
-    .then(() =>app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+mongoose.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
