597b4ff3f964932b30d401e0958b59df4a6c1346
gennttllee <udooto72@gmail.com>
2022-08-07 07:44:39 +0100

    done

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
     origin: '*'
