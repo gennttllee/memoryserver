fe58313067d931b2744c9eeac73ad98f483eaa93
gennttllee <udooto72@gmail.com>
2022-08-07 05:22:29 +0100

    o

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
-        'Access-Control-Allow-Origin',
-        "Origin, X-Requested-With, Content-Type, Accept"
+        'Access-Control-Allow-Origin'
     );
     next();
 });
