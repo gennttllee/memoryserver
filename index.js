6b8fdc6ad8f08cf8b5fcf4771f1e844ba2538ab0
gennttllee <udooto72@gmail.com>
2022-08-07 02:12:06 +0100

    still cors erroor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
+import  express  from "express";
+import  bodyParser  from "body-parser";
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
@@ -9,10 +9,10 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({limit : '30mb', extended : true}));
+app.use(bodyParser.urlencoded({limit : '30mb', extended : true}));
 app.use(cors({
-    origin: '*'
+    origin : 'https://memory-gennttllee.vercel.app/'
 }));
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
@@ -21,7 +21,7 @@ const CONNECTION_URL = process.env.CONNECTION;
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+mongoose.connect(CONNECTION_URL,{useUnifiedTopology : true, useNewUrlParser: true })
+    .then(() =>app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
